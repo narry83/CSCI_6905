d6c3fb5ae9e988a50dbf802dd87f2db98074b2fb
Narsimha N <narayankhedkarn19@students.ecu.edu>
2021-05-19 09:12:33 -0400

    EndofChapter6

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.geoquiz
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -12,6 +14,7 @@ import androidx.lifecycle.ViewModelProviders
 
 private const val TAG ="MainActivity"
 private const val KEY_INDEX ="index"
+private const val REQUEST_CODE_CHEAT = 0
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
+    private lateinit var cheatButton: Button
 
     private val quizViewModel : QuizViewModel by lazy{
         ViewModelProviders.of(this).get(QuizViewModel::class.java)
@@ -39,6 +43,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        cheatButton = findViewById(R.id.cheat_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
@@ -52,6 +57,15 @@ class MainActivity : AppCompatActivity() {
         nextButton.setOnClickListener {
             quizViewModel.moveToNext()
             updateQuestion()
+        }
+
+        cheatButton.setOnClickListener{
+            //Start
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            startActivityForResult(intent, REQUEST_CODE_CHEAT )
+
+
         }
 
         updateQuestion()
@@ -88,16 +102,29 @@ class MainActivity : AppCompatActivity() {
         savedInstanceState.putInt(KEY_INDEX,quizViewModel.currentIndex)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode!= Activity.RESULT_OK){
+            return
+        }
+
+        if (requestCode == REQUEST_CODE_CHEAT){
+            quizViewModel.isCheater=data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+        }
+    }
+
     private fun updateQuestion() {
         questionTextView.setText( quizViewModel.currentQuestionText)
     }
 
     private fun checkAnswer(userAnswer: Boolean){
         val correctAnswer =quizViewModel.currentQuestionAnswer
-        val messageResId= if(userAnswer == correctAnswer){
-            R.string.correct_toast
-        }else{
-            R.string.incorrect_toast
+
+        val messageResId = when{
+            quizViewModel.isCheater -> R.string.judgment_toast
+            userAnswer==correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
         }
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
